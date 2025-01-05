05a9151f33c2826286bdc1a4f33aae3307068a9e
Pulkit Singh <pulkitsingh01@gmail.com>
2025-01-06 01:09:36 +0530

    .

--- src/views/change-plan-view/logic/handleSubmitPlanRequest.ts
+++ src/views/change-plan-view/logic/handleSubmitPlanRequest.ts
@@ -1,6 +1,9 @@
 import { AGENTS } from "@/constants/agents.constants";
 import { ClientToServerChannel } from "@/ipc/channels.enum";
-import {setChangePlanViewState as setState, setChangePlanViewState} from '@/views/change-plan-view/store/change-plan-view.logic';
+import {
+  setChangePlanViewState as setState,
+  setChangePlanViewState,
+} from "@/views/change-plan-view/store/change-plan-view.logic";
 import { getChangePlanViewState } from "@/views/change-plan-view/store/change-plan-view.store";
 import { getSelectedFiles } from "@/views/change-plan-view/logic/getSelectedFiles";
 import { ClientPostMessageManager } from "@/ipc/client-ipc";
@@ -15,35 +18,43 @@ export const handleSubmitPlanRequest = (
   const changeDescription = getChangePlanViewState("changeDescription");
   if (!changeDescription) {
     setState("isLoading")(false);
-    const fileChunkMap = getChangePlanViewState('fileChunkMap');
-    const updatedFileChunkMap = Object.keys(fileChunkMap).reduce((acc, filePath) => {
-      acc[filePath] = {
-        ...fileChunkMap[filePath],
-        isLoading: false,
-      };
-      return acc;
-    }, {});
-    setChangePlanViewState('fileChunkMap')(updatedFileChunkMap);
+    const fileChunkMap = getChangePlanViewState("fileChunkMap");
+    const updatedFileChunkMap = Object.keys(fileChunkMap).reduce(
+      (acc, filePath) => {
+        acc[filePath] = {
+          ...fileChunkMap[filePath],
+          isLoading: false,
+        };
+        return acc;
+      },
+      {}
+    );
+    setChangePlanViewState("fileChunkMap")(updatedFileChunkMap);
     return;
   }
 
   const selectedFiles = getSelectedFiles(files);
-  const isUpdatingPlan = getChangePlanViewState("chatHistory").length  && llmResponse
+  const isUpdatingPlan =
+    getChangePlanViewState("chatHistory").length && llmResponse;
   const newChatHistory = [
-    ...(isUpdatingPlan ? [
-          {
-            user: "instructor",
-            message: AGENTS["Code Plan"]?.systemInstructions,
-          },
-        ]
-      : [
+    ...(isUpdatingPlan
+      ? [
           ...getChangePlanViewState("chatHistory"), // Use chatHistory from store
           {
             user: "instructor",
             message: AGENTS["Code Plan Update"]?.systemInstructions,
           },
+        ]
+      : [
+          {
+            user: "instructor",
+            message: AGENTS["Code Plan"]?.systemInstructions,
+          },
         ]),
-    { user: "user", message: (isUpdatingPlan ? `Revise the plan:\n` : '') + changeDescription },
+    {
+      user: "user",
+      message: (isUpdatingPlan ? `Revise the plan:\n` : "") + changeDescription,
+    },
   ];
   setState("chatHistory")(newChatHistory); // Update chatHistory in the store
 
